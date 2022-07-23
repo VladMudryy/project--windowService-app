74f0c44beb2edd4f1e7b4b0bb9e64d83b176f26c
Vlad_Mudryy <vladyslav.mudryy@gmail.com>
2022-07-23 03:10:19 +0300

    chanage structure of tabs_js

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,6 @@ import tabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
     modal();
-    tabs();
+    tabs('.glazing_block', '.glazing_content', 'active');
+    tabs('.no_click', '.decoration_content-item', 'after_click');
 });
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,38 +1,33 @@
-function tabs() {
-    function createTabs(tabTrigger, tabContent, activeClass) {
-        const trigger = document.querySelectorAll(tabTrigger),
-              tab = document.querySelectorAll(tabContent);
+function tabs(tabTrigger, tabContent, activeClass) {
+    const trigger = document.querySelectorAll(tabTrigger),
+            tab = document.querySelectorAll(tabContent);
 
-        function hideContent() {
-            trigger.forEach(item => {
-                item.classList.remove(activeClass);
-            });
-            tab.forEach(item => {
-                item.style.display = 'none';
-            });
-        }
-
-        function showContent(i = 0) {
-            trigger[i].classList.add(activeClass);
-            tab[i].style.display = 'block';
-        } 
-        
-        hideContent();
-        showContent();
-
-        trigger.forEach((item, i) => {
-            item.addEventListener('click', event => {
-                event.preventDefault();
-                if (event.target.closest('a') || event.target.closest('img')) {
-                    hideContent();
-                    showContent(i);
-                }
-            });
+    function hideContent() {
+        trigger.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+        tab.forEach(item => {
+            item.style.display = 'none';
         });
     }
 
-    createTabs('.glazing_block', '.glazing_content', 'active');
-    createTabs('.decoration_item > div', '.decoration_content-item', 'after_click');
+    function showContent(i = 0) {
+        trigger[i].classList.add(activeClass);
+        tab[i].style.display = 'block';
+    } 
+    
+    hideContent();
+    showContent();
+
+    trigger.forEach((item, i) => {
+        item.addEventListener('click', event => {
+            event.preventDefault();
+            if (event.target.closest('a') || event.target.closest('img')) {
+                hideContent();
+                showContent(i);
+            }
+        });
+    });
 }
 
 export default tabs;
