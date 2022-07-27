e70e4f571c134a0ac04fc0fbb7882b71b706673b
Vlad <vladyslav.mudryytech@gmail.com>
2022-07-27 19:37:30 +0300

    fix scroll bug with modals

--- src/js/services/services.js
+++ src/js/services/services.js
@@ -1,11 +1,30 @@
+function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+}
+
+const scrollRight = calcScroll();
+
 function openModal(modalClass, activeClass = 'modal-open') {
     modalClass.style.display = 'block';
     document.querySelector('body').classList.add(activeClass);
+    document.querySelector('body').style.marginRight = `${scrollRight}px`;
 }
 
 function closeModal(modalClass, activeClass = 'modal-open') {
     modalClass.style.display = 'none';
     document.querySelector('body').classList.remove(activeClass);
+    document.querySelector('body').style.marginRight = `0px`;
 }
 
 function checkDigits(selector) {
